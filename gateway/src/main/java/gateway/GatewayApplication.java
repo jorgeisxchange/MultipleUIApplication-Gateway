b42e54f268f713f15d093ad01eeb4907c268e13e
jorgeisxchange <jorgei@sxchange.com.ph>
2015-09-09 14:54:04 +0800

    starter files for the section
    setup and configured multiple ui and gateway section starter files from
    scratch using spring initializer

---
+++ gateway/src/main/java/gateway/GatewayApplication.java
@@ -0,0 +1,16 @@
+package gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@SpringBootApplication
+@EnableRedisHttpSession
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ resource/src/main/java/resource/ResourceApplication.java
@@ -0,0 +1,38 @@
+package resource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionStrategy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableRedisHttpSession
+public class ResourceApplication {
+	
+	@RequestMapping("/")
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		
+		return model;
+	}	
+	
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceApplication.class, args);
+    }
+    
+    
+    @Bean
+    public HeaderHttpSessionStrategy sessionStrategy() {
+    	return new HeaderHttpSessionStrategy();
+    }
+}
