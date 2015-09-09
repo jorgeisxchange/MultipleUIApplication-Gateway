8c8451a2e20d2a99de7d41f4ab2bcd09420ae256
jorgeisxchange <jorgei@sxchange.com.ph>
2015-09-09 17:28:38 +0800

    fixes - up and running

--- resource/src/main/java/resource/ResourceApplication.java
+++ resource/src/main/java/resource/ResourceApplication.java
@@ -6,16 +6,17 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableRedisHttpSession
-public class ResourceApplication {
+public class ResourceApplication extends WebSecurityConfigurerAdapter {
 	
 	@RequestMapping("/")
 	public Map<String, Object> home() {
@@ -30,9 +31,9 @@ public class ResourceApplication {
         SpringApplication.run(ResourceApplication.class, args);
     }
     
-    
-    @Bean
-    public HeaderHttpSessionStrategy sessionStrategy() {
-    	return new HeaderHttpSessionStrategy();
+    @Override
+	protected void configure(HttpSecurity http) throws Exception {
+    	http.httpBasic().disable();
+        http.authorizeRequests().anyRequest().authenticated();	
     }
 }
--- ui/src/main/java/ui/UiApplication.java
+++ ui/src/main/java/ui/UiApplication.java
@@ -2,6 +2,11 @@ package ui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
@@ -11,4 +16,18 @@ public class UiApplication {
     public static void main(String[] args) {
         SpringApplication.run(UiApplication.class, args);
     }
+    
+    @Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.httpBasic().and()
+				.authorizeRequests()
+					.antMatchers("/index.html", "/").permitAll()
+					.anyRequest().authenticated();
+		}
+	}
+    
 }
